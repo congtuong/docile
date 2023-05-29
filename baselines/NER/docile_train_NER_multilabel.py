1a664692cd3bdcaf03289df50a75830e2943a581
congtuong <132115321+congtuong@users.noreply.github.com>
2023-05-29 23:10:58 +0700

    Update docile_train_NER_multilabel.py

--- baselines/NER/docile_train_NER_multilabel.py
+++ baselines/NER/docile_train_NER_multilabel.py
@@ -8,7 +8,8 @@ from datetime import datetime
 from pathlib import Path
 
 from tensorboard import program
-
+import sys
+sys.path.append('.')
 
 import numpy as np
 import torch
@@ -23,68 +24,70 @@ from transformers import AutoTokenizer, Trainer, TrainingArguments
 from transformers.models.lilt.configuration_lilt import LiltConfig
 from docile.dataset import KILE_FIELDTYPES, LIR_FIELDTYPES, Dataset
 from torch.optim.lr_scheduler import StepLR
+import sys
+
 # import torch.optim.lr_scheduler.StepLR
-class SAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
-        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+# class SAM(torch.optim.Optimizer):
+#     def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
+#         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
 
-        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
-        super(SAM, self).__init__(params, defaults)
+#         defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
+#         super(SAM, self).__init__(params, defaults)
 
-        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
-        self.param_groups = self.base_optimizer.param_groups
-        self.defaults.update(self.base_optimizer.defaults)
+#         self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+#         self.param_groups = self.base_optimizer.param_groups
+#         self.defaults.update(self.base_optimizer.defaults)
 
-    @torch.no_grad()
-    def first_step(self, zero_grad=False):
-        grad_norm = self._grad_norm()
-        for group in self.param_groups:
-            scale = group["rho"] / (grad_norm + 1e-12)
+#     @torch.no_grad()
+#     def first_step(self, zero_grad=False):
+#         grad_norm = self._grad_norm()
+#         for group in self.param_groups:
+#             scale = group["rho"] / (grad_norm + 1e-12)
 
-            for p in group["params"]:
-                if p.grad is None: continue
-                self.state[p]["old_p"] = p.data.clone()
-                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
-                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+#             for p in group["params"]:
+#                 if p.grad is None: continue
+#                 self.state[p]["old_p"] = p.data.clone()
+#                 e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+#                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
 
-        if zero_grad: self.zero_grad()
+#         if zero_grad: self.zero_grad()
 
-    @torch.no_grad()
-    def second_step(self, zero_grad=False):
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None: continue
-                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
+#     @torch.no_grad()
+#     def second_step(self, zero_grad=False):
+#         for group in self.param_groups:
+#             for p in group["params"]:
+#                 if p.grad is None: continue
+#                 p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
 
-        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+#         self.base_optimizer.step()  # do the actual "sharpness-aware" update
 
-        if zero_grad: self.zero_grad()
+#         if zero_grad: self.zero_grad()
         
     
-    @torch.no_grad()
-    def step(self, closure=None):
-        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
-        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+#     @torch.no_grad()
+#     def step(self, closure=None):
+#         assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+#         closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
         
-        self.first_step(zero_grad=True)
-        closure()
-        self.second_step()
+#         self.first_step(zero_grad=True)
+#         closure()
+#         self.second_step()
 
-    def _grad_norm(self):
-        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
-        norm = torch.norm(
-                    torch.stack([
-                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
-                        for group in self.param_groups for p in group["params"]
-                        if p.grad is not None
-                    ]),
-                    p=2
-               )
-        return norm
+#     def _grad_norm(self):
+#         shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+#         norm = torch.norm(
+#                     torch.stack([
+#                         ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
+#                         for group in self.param_groups for p in group["params"]
+#                         if p.grad is not None
+#                     ]),
+#                     p=2
+#                )
+#         return norm
 
-    def load_state_dict(self, state_dict):
-        super().load_state_dict(state_dict)
-        self.base_optimizer.param_groups = self.param_groups
+#     def load_state_dict(self, state_dict):
+#         super().load_state_dict(state_dict)
+#         self.base_optimizer.param_groups = self.param_groups
 
 classes = (
     [
@@ -965,9 +968,9 @@ if __name__ == "__main__":
             args.model_name, config=config
         )
 
-    base_optimizer = torch.optim.SGD
-    optimizer = SAM(model.parameters(), base_optimizer, lr=2e-5, momentum=0.9)
-    scheduler = StepLR(optimizer, args.lr, args.num_epochs)
+    # base_optimizer = torch.optim.SGD
+    # optimizer = SAM(model.parameters(), base_optimizer, lr=2e-5, momentum=0.9)
+    # scheduler = StepLR(optimizer, args.lr, args.num_epochs)
     training_args = TrainingArguments(
         output_dir=os.path.join(args.output_dir),
         evaluation_strategy="epoch",
@@ -1084,8 +1087,8 @@ if __name__ == "__main__":
         eval_dataset=val_dataset,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        optimizers = ( optimizer,scheduler)
+        compute_metrics=compute_metrics
+        # optimizers = ( optimizer,scheduler)
     )
     
     train_result = trainer.train()
